e809d40fb4e0faf41ab183d83999b34b0f9ec1a7
Michael Pace <terinopace@gmail.com>
2022-03-19 23:43:22 -0600

    Create wrapper for NIO based HTTPMethods, Method
    - create public protocol, Verifier, to query for requests made during an event loop

---
+++ Sources/Sails/Method.swift
@@ -0,0 +1,19 @@
+//
+// Created by Michael Pace on 3/19/22.
+//
+
+import Foundation
+import NIOHTTP1
+
+public enum Method {
+    case GET
+    case POST
+
+    init(rawValue: HTTPMethod) {
+        switch rawValue {
+        case HTTPMethod.GET: self = Method.GET
+        case HTTPMethod.POST: self = Method.POST
+        default: self = Method.GET
+        }
+    }
+}
---
+++ Sources/Sails/Verifier.swift
@@ -0,0 +1,9 @@
+//
+// Created by Michael Pace on 3/19/22.
+//
+
+import Foundation
+
+public protocol Verifier {
+    func verify(_ method: Method, _ uri: String) -> Int
+}
